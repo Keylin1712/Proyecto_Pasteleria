0e6290ccc8c28c5a345763f0fc5cb739cde21a06
Keylin Ramírez Ruiz <130193620+Keylin1712@users.noreply.github.com>
2023-07-05 16:45:14 -0600

    Segundo avance del proyecto

---
+++ Proyecto_Pateleria/src/test/java/com/proyecto_pasteleria/ProyectoPasteleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_pasteleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPasteleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
